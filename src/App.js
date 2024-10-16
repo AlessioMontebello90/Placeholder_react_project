a77ba6986c7c5eced894f30f84306c1018915811
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2024-10-16 14:27:12 +0200

    add poststable .js & .css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import PostsTable from './components/PostsTable/PostsTable';
 import './App.css';
+import { AppBar, Toolbar, Typography, Container } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Barra di navigazione */}
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div">
+            JSONPlaceholder App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      
+      <Container>
+        <PostsTable />
+      </Container>
     </div>
   );
 }
