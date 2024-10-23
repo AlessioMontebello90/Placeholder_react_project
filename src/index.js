fcc381f4d8a352eac7bd224043cc3b3f7467c3f0
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2024-10-23 09:27:57 +0200

    remove <React.StrictMode>

--- src/index.js
+++ src/index.js
@@ -6,9 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // Rimuovi temporaneamente React.StrictMode per evitare doppie chiamate API
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
